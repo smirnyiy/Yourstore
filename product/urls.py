3f31792018df3ec4c66925d91528a2cad11f92b7
ANDREY <smirnovandrey@list.ru>
2024-02-28 16:50:14 +0300

    add virtual env

--- product/urls.py
+++ product/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [path('', IndexView.as_view(), name = 'index'),
                path('page/<int:page>/', ProductsListView.as_view(), name='paginator'),
                path('baskets/add/<int:product_id>/', basket_add, name='basket_add'),
                path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
+
                ]
